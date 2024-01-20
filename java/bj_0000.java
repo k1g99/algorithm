5e940f5447b3d657d1cda6742f43cc66995f6959
k1g99 <idmatthew9546@gmail.com>
2024-01-20 14:30:22 +0900

    백준 template - java

---
+++ java/bj_0000.java
@@ -0,0 +1,21 @@
+package java;
+
+import java.io.*;
+import java.util.*;
+
+public class bj_0000 {
+    public static void main(String[] args) throws IOException {
+        // input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // algorithm
+        int answer = 0;
+
+
+
+        // output
+        System.out.println(answer);
+        br.close();
+    }
+}
