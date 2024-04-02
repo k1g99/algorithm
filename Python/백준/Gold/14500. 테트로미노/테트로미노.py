738f61ab8021ba379c001f5652878e2aaf8b481d
김일건 <100272259+k1g99@users.noreply.github.com>
2024-04-03 00:41:01 +0900

    [Gold IV] Title: 테트로미노, Time: 216 ms, Memory: 39092 KB -BaekjoonHub

--- Python/백준/Gold/14500. 테트로미노/테트로미노.py
+++ Python/백준/Gold/14500. 테트로미노/테트로미노.py
@@ -4,15 +4,27 @@ dir = [[-1, 0], [1,0], [0,1], [0, -1]]
 answer = 0
 visited = [[False for _ in range(C+2)] for _ in range(R+2)]
 part_sum = 0
+max_val = 0
 
 def dfs(r, c, n, s):
     global answer
+    if(n == 0 and s + max_val * 3 < answer):
+        return 
+    if(n == 1 and s + max_val * 2 < answer):
+        return 
+    if(n == 2 and s + max_val * 1 < answer):
+        return 
     if(n == 3):
         answer = max(answer, s)
         return
+        
     for d in dir:
         nr, nc = r + d[0], c + d[1]
         if(nr > 0 and nc > 0 and nr <= R and nc <= C and not visited[nr][nc]):
+            if(n == 1):
+                visited[nr][nc] = True
+                dfs(r, c, n+1, s + mat[nr][nc])
+                visited[nr][nc] = False 
             visited[nr][nc] = True
             dfs(nr, nc, n+1, s + mat[nr][nc])
             visited[nr][nc] = False
@@ -21,6 +33,7 @@ mat.append([0] * (C+2))
 for r in range(R):
     row = [0]
     row += list(map(int, input().split()))
+    max_val = max(max_val, max(row))
     row += [0]
     mat.append(row)
 mat.append([0] * (C+2))
@@ -31,14 +44,5 @@ for r in range(1, R+1):
         dfs(r, c, 0, mat[r][c])
         visited[r][c] = False
 
-        cross_sum = mat[r][c]
-        edges = []
-        for d in dir:
-            edges.append(mat[r+d[0]][c+d[1]])
-        edges.sort()
-        cross_sum += sum(edges[1:])
-        answer = max(answer, cross_sum)
-
-        
 
 print(answer)
