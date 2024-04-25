13c9318072f47a7c9b1c819f7b0a3e382ad5ecde
김일건 <100272259+k1g99@users.noreply.github.com>
2024-04-26 00:10:11 +0900

    [Gold V] Title: AC, Time: 240 ms, Memory: 134120 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/5430. AC/AC.py
@@ -0,0 +1,52 @@
+def solve():
+    cmd = input()
+    arr_len = int(input())
+    arr_str = input()
+    dir = True
+
+    # 여기서 일단 arr_len보다 cmd에서 'D'가 더 많으면 error출력하고 끝
+    count_D = cmd.count('D')
+    if(count_D>arr_len):
+        print('error')
+        return
+    elif(count_D == arr_len):
+        print('[]')
+        return
+
+    if(arr_str == '[]'):
+        print(arr_str)
+        return 
+
+    arr = arr_str[1:-1]
+    arr = list(map(int, arr.split(",")))
+    # print(arr)
+
+    start = 0
+    end = len(arr)
+
+    for c in cmd:
+        if(c == 'R'):
+            dir = not dir
+        elif(c == 'D'):
+            if(dir):
+                start += 1
+            else:
+                end -= 1
+    
+    print('[', end="")
+    if(dir):
+        while(start < end-1):
+            print(arr[start], end=',')
+            start += 1
+        print(arr[end-1], end=']')
+    else:
+        while(end > start + 1):
+            end -= 1
+            print(arr[end], end=',')
+        print(arr[start], end=']') 
+    print()
+
+
+N = int(input())
+for _ in range(N):
+    solve()
