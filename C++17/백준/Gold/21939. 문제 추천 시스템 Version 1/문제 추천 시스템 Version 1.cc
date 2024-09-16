e9d2fb8bc5d52e6cb5bd47fd65857e31c33adb34
김일건 <100272259+k1g99@users.noreply.github.com>
2024-09-16 22:15:31 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 124 ms, Memory: 16156 KB -BaekjoonHub

---
+++ C++17/백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int N, P, L, M; // 문제 개수, 문제 번호, 난이도, 명령어 개수
+string cmd;
+int X;
+// multimap<int, int, greater<int> > prob; // key만 정렬이 되나봄...?
+
+multiset<pair<int, int>, std::greater<pair<int, int> > > hardProb;
+multiset<pair<int, int> > easyProb;
+map<int, int> prob; // {문제번호, 난이도}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N;
+    for (int n = 0; n < N; n++){
+        cin >> P >> L;
+        hardProb.insert(make_pair(L, P));
+        easyProb.insert(make_pair(L, P));
+        prob.insert(make_pair(P, L));
+    }
+
+    cin >> M;
+    for (int m = 0; m < M; m++){
+        cin >> cmd; 
+
+        if( cmd == "add") {
+            cin >> P >> L;
+            hardProb.insert(make_pair(L, P));
+            easyProb.insert(make_pair(L, P));
+            prob.insert(make_pair(P, L));
+        }
+        else if(cmd == "recommend"){
+            cin >> X;
+            pair<int, int> target;
+            if(X == 1){
+                target = make_pair(hardProb.begin()->first, hardProb.begin()->second);
+                cout << target.second << "\n";
+            }else{
+                target = make_pair(easyProb.begin()->first, easyProb.begin()->second);
+                cout << target.second << "\n";
+            }
+        }else if(cmd == "solved"){
+            // prob에서 문제, 난이도 찾고 지우기
+            cin >> X; // X = 문제번호
+            pair<int, int> target = make_pair(prob[X], X);
+            prob.erase(X);
+
+            // 난이도Prob에서 난이도, 문제 찾고 지우기
+            hardProb.erase(target);
+            easyProb.erase(target);
+        }
+    }
+
+    return 0;
+}
