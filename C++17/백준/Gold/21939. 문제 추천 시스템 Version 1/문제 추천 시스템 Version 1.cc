d15f61f536d941908d1f6970122eaee83034d98d
김일건 <100272259+k1g99@users.noreply.github.com>
2024-09-16 22:35:41 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 24 ms, Memory: 5236 KB -BaekjoonHub

--- C++17/백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.cc
+++ C++17/백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.cc
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
-#include <map>
-#include <set>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
 int N, P, L, M; // 문제 개수, 문제 번호, 난이도, 명령어 개수
 string cmd;
 int X;
-// multimap<int, int, greater<int> > prob; // key만 정렬이 되나봄...?
+int prob[100001]; // 값이 1 이상 이면 관리 중인 문제 (문제 번호는 unique함)
 
-multiset<pair<int, int>, std::greater<pair<int, int> > > hardProb;
-multiset<pair<int, int> > easyProb;
-map<int, int> prob; // {문제번호, 난이도}
+priority_queue<pair<int, int> > hardProb;
+priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > easyProb;
+
 
 int main(){
     ios::sync_with_stdio(0);
@@ -22,9 +22,9 @@ int main(){
     cin >> N;
     for (int n = 0; n < N; n++){
         cin >> P >> L;
-        hardProb.insert(make_pair(L, P));
-        easyProb.insert(make_pair(L, P));
-        prob.insert(make_pair(P, L));
+        hardProb.push(make_pair(L, P));
+        easyProb.push(make_pair(L, P));
+        prob[P] = L;
     }
 
     cin >> M;
@@ -33,26 +33,27 @@ int main(){
 
         if( cmd == "add") {
             cin >> P >> L;
-            hardProb.insert(make_pair(L, P));
-            easyProb.insert(make_pair(L, P));
-            prob.insert(make_pair(P, L));
+            hardProb.push(make_pair(L, P));
+            easyProb.push(make_pair(L, P));
+            prob[P] = L;
         }
         else if(cmd == "recommend"){
             cin >> X;
             if(X == 1){
-                cout << hardProb.begin()->second << "\n";
+                // {난이도, 문제번호}
+                while(prob[hardProb.top().second] != hardProb.top().first){
+                    hardProb.pop();
+                }
+                cout << hardProb.top().second << "\n";
             }else{
-                cout << easyProb.begin()->second << "\n";
+                while(prob[easyProb.top().second] != easyProb.top().first){
+                    easyProb.pop();
+                }
+                cout << easyProb.top().second << "\n";
             }
         }else if(cmd == "solved"){
-            // prob에서 문제, 난이도 찾고 지우기
             cin >> X; // X = 문제번호
-            pair<int, int> target = make_pair(prob[X], X);
-            prob.erase(X);
-
-            // 난이도Prob에서 난이도, 문제 찾고 지우기
-            hardProb.erase(target);
-            easyProb.erase(target);
+            prob[X] = 0;
         }
     }
 
